a24389933009f8c00eadd14e23ce764ecf011cee
usa6463 <usa6463@gmail.com>
2021-06-25 23:42:22 +0900

    1. 디씨 게시글 파싱 로직 추가 title content nickname ip date viewCount recommendCount commentCount

--- src/main/java/DCCrawler.java
+++ src/main/java/DCCrawler.java
@@ -4,16 +4,21 @@ import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class DCCrawler extends WebCrawler {
-    public static final String HOST = "https://gall.dcinside.com/";
+    public static final String HOST = "https://gall.dcinside.com/board/view/?id=neostock&no=";
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
+            + "|png|mp3|mp4|zip|gz))$");
 
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
-        String href = url.getURL();
-        logger.debug("shouldVisit href: {}", href);
-        return href.startsWith(HOST);
+        String href = url.getURL().toLowerCase();
+        return !FILTERS.matcher(href).matches()
+                && href.startsWith(HOST);
     }
 
     @Override
@@ -48,10 +53,28 @@ public class DCCrawler extends WebCrawler {
             // 페이지의 HTML을 파싱하기
             Document doc = Jsoup.parse(html);
 
-            // 내용만 추출하기
-            String content = doc.select("div#writing_view_box").html();
 
-            logger.debug("div#content_text : {}", content);
+            String content = doc.select(".write_div").html();
+            String title = doc.select(".title_subject").html();
+
+            Elements fl = doc.select(".fl");
+            String nickname = fl.select(".nickname").html();
+            String ip = fl.select(".ip").html();
+            String date = fl.select(".gall_date").html();
+
+            Elements fr = doc.select(".fr");
+            String viewCount = fr.select(".gall_count").html();
+            String recommendCount = fr.select(".gall_reply_num").html();
+            String commentCount = fr.select(".gall_comment").html();
+
+            logger.debug("title : {}", title);
+            logger.debug("content : {}", content);
+            logger.debug("nickname : {}", nickname);
+            logger.debug("ip : {}", ip);
+            logger.debug("date : {}", date);
+            logger.debug("viewCount : {}", viewCount);
+            logger.debug("recommendCount : {}", recommendCount);
+            logger.debug("commentCount : {}", commentCount);
         }
     }
 }
