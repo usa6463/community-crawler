d4f82393b69cdcfbd1664fd45af8f38473139f7a
usa6463 <usa6463@gmail.com>
2021-07-13 22:53:04 +0900

    1. lastContentNum, latestContentNum 로깅 추가

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,15 +3,17 @@ import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.util.stream.IntStream;
 
 public class Controller {
     public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(Controller.class);
 
         int latestContentNum = DCCrawler.getLatestContentNum("https://gall.dcinside.com/board/lists?id=neostock");
-
         int numberOfCrawlers = 5;
 
         CrawlConfig config = new CrawlConfig();
@@ -19,7 +21,6 @@ public class Controller {
         config.setPolitenessDelay(300);				// 동일 호스트에 대한 요청 delay 설정 (ms)
         config.setCrawlStorageFolder("data/crawl");	// 크롤러의 데이터 저장 디렉터리 지정
 
-        // CrawController 준비하기
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
@@ -30,17 +31,14 @@ public class Controller {
         System.out.println(targetDate);
 
         int lastContentNum = 1272610;
+        logger.info("lastContentNum : {}, latestContentNum : {}", lastContentNum, latestContentNum);
 
         // 크롤링 시작 URL 지정하기
         IntStream.range(lastContentNum, latestContentNum+1)
-                .forEach(contentNum-> { controller.addSeed(String.format("https://gall.dcinside.com/board/view/?id=neostock&no=%d", contentNum)); });
+                .forEach(contentNum-> controller.addSeed(String.format("https://gall.dcinside.com/board/view/?id=neostock&no=%d", contentNum)));
 
         CrawlController.WebCrawlerFactory<DCCrawler> factory = () -> new DCCrawler(targetDate);
         // 크롤링 시작하기
         controller.start(factory, numberOfCrawlers);
-
-
-
-
     }
 }
