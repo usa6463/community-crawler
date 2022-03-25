c1edc58d868a6cb0b64d05a5438bcb6142ab37fa
usa6463 <usa6463@gmail.com>
2022-03-25 15:11:20 +0900

    UPDATE Runner job 실행 후 서버 종료되도록 수정

--- src/main/java/org/example/community/crawler/Application.java
+++ src/main/java/org/example/community/crawler/Application.java
@@ -1,14 +1,17 @@
 package org.example.community.crawler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args).close();
+        log.info("System successfully shut down");
     }
 
 }
