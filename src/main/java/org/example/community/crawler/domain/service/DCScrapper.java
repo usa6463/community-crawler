4458822c6c548c75fe1f9f6a95747e844a6a8391
usa6463 <usa6463@gmail.com>
2022-03-25 14:49:59 +0900

    UPDATE DCScrapper.java 리팩토링

--- src/main/java/org/example/community/crawler/domain/service/DCScrapper.java
+++ src/main/java/org/example/community/crawler/domain/service/DCScrapper.java
@@ -20,17 +20,27 @@ import java.util.stream.IntStream;
 @Service
 @Slf4j
 public class DCScrapper {
+
+    /**
+     * DC 게시판에서 확인할 수 있는 게시글 등록일자 포맷
+     */
+    public static final String DC_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    /**
+     * 게시판 번호 붙이는 포맷
+     */
+    public static final String DC_BOARD_PAGE_URL_FORMAT = "%s&page=%d";
+
     /**
      * DC 인사이드 커뮤니티 사이트의 특정날짜 게시글을 스크래핑 하여 스토리지에 저장
      */
     public void scrap() {
         log.info("DCScrapper start");
 
-        String targetDateStr = "2022-03-22";
+        String targetDateStr = "2022-03-23";
         LocalDate targetDate = LocalDate.parse(targetDateStr);
 
         try {
-            traverseBoard(targetDate);
+            traverseBoard(targetDate, "https://gall.dcinside.com/board/lists/?id=rlike");
             // 각 게시글 대상으로 스크래핑 및 ES 저장
             // rate limiter 적용해서 요청 쓰로틀링 필요
         } catch (Exception e) {
@@ -40,21 +50,27 @@ public class DCScrapper {
 
     /**
      * 게시판 순회. target_date 이전 날짜 나오기 시작하면 중단
+     *
      * @param targetDate 수집대상 게시글 작성일
-     * @throws IOException
+     * @param boardBaseUrl 순회 대상 게시판 URL(페이지 번호 제외)
      * @return 게시판 순회하면서 target_date 날짜 게시글 링크를 list에 append 후 반환
+     * @throws IOException
      */
-    private List<DCPost> traverseBoard(LocalDate targetDate) throws IOException, InterruptedException {
+    private List<DCPost> traverseBoard(LocalDate targetDate, String boardBaseUrl) throws IOException, InterruptedException {
         int boardPage = 1;
         Boolean targetDateFlag = true;
         List<DCPost> result = new ArrayList<>();
         while (targetDateFlag) {
-            List<DCPost> list = getDcBoards(boardPage, "https://gall.dcinside.com/board/lists/?id=neostock&page=%d");
-            long count = list.stream().filter(post -> LocalDate.parse(post.getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).isBefore(targetDate)).count();
-            if (count > 0) {
+            String boardUrl = String.format(DC_BOARD_PAGE_URL_FORMAT, boardBaseUrl, boardPage);
+            List<DCPost> list = getDcPosts(boardUrl);
+            long targetDateBeforePostCount = list.stream()
+                    .filter(post -> LocalDate.parse(post.getDate(), DateTimeFormatter.ofPattern(DC_DATETIME_FORMAT))
+                            .isBefore(targetDate))
+                    .count();
+            if (targetDateBeforePostCount > 0) {
                 targetDateFlag = false;
             }
-            log.info("Count : {}", count);
+            log.info("targetDateBeforePostCount : {}", targetDateBeforePostCount);
             log.info("{}", list);
             result.addAll(list);
             boardPage = boardPage + 1;
@@ -65,13 +81,14 @@ public class DCScrapper {
 
     /**
      * DC 게시판을 파싱하여 게시글 정보 수집
-     * @param boardPageNum 페이지 번호
-     * @param boardUrl 스크래핑할 페이지 URL
+     *
+     * @param boardUrl     스크래핑할 페이지 URL
      * @return 게시글 정보 리스트
      * @throws IOException
      */
-    private List<DCPost> getDcBoards(int boardPageNum, String boardUrl) throws IOException {
-        Document doc = Jsoup.connect(String.format(boardUrl, boardPageNum)).get();
+    private List<DCPost> getDcPosts(String boardUrl) throws IOException {
+        log.info("get Post info from {}", boardUrl);
+        Document doc = Jsoup.connect(boardUrl).get();
 
         Elements gallDateList = doc.select(".gall_date");
         Elements gallNumList = doc.select(".gall_num");
