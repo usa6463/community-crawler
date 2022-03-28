ac0048ba4b8d612cbfebff76450beeef89b87cdc
usa6463 <usa6463@gmail.com>
2022-03-28 21:54:45 +0900

    UPDATE ElasticsearchConfig 추가

---
+++ src/main/java/org/example/community/crawler/repository/elasticsearchRepository.java
@@ -0,0 +1,7 @@
+//package org.example.community.crawler.repository;
+//
+//import org.example.community.crawler.domain.entity.DCContent;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+//public interface elasticsearchRepository extends ElasticsearchRepository<DCContent, String> {
+//}
