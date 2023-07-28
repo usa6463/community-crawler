87b64405333a053606ba8d9ab90ba253c761a11a
usa6463 <usa6463@gmail.com>
2023-07-28 19:46:54 +0900

    Revert "UPDATE CommonScrapperFunction logging"
    This reverts commit da667712144f2482c6157574d737367cb433a077.

--- src/main/java/org/example/community/crawler/domain/service/CommonScrapperFunction.java
+++ src/main/java/org/example/community/crawler/domain/service/CommonScrapperFunction.java
@@ -1,6 +1,5 @@
 package org.example.community.crawler.domain.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.example.community.crawler.config.AppConfiguration;
 import org.jsoup.Jsoup;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +11,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
 public class CommonScrapperFunction {
 
     public static WebDriver getWebDriver(AppConfiguration appConfiguration, String webDriverId) {
@@ -25,7 +23,6 @@ public class CommonScrapperFunction {
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--single-process");
         chromeOptions.addArguments("--disable-dev-shm-usage");
-        log.debug("chromedriver option setting complete");
 
         driver = new ChromeDriver(chromeOptions);
         return driver;
